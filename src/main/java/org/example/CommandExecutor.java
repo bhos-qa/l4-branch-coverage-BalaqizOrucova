497e916c4dea4a2be4254c1343172ec14e1b0aed
Balaqiz Orucova <balagiz.orujova1.std@bhos.edu.az>
2023-09-30 12:35:25 +0400

    final

--- src/main/java/org/example/CommandExecutor.java
+++ src/main/java/org/example/CommandExecutor.java
@@ -5,13 +5,39 @@ import java.util.Map;
 
 public class CommandExecutor {
 
-    // Vulnerable method with hardcoded password
-    public String authenticate(String userProvidedPassword) {
-        String hardcodedPassword = "password123";
-        if (userProvidedPassword.equals(hardcodedPassword)) {
-            return "Authentication successful!";
-        } else {
-            return "Authentication failed!";
+    public void processUserData(String username, String password) {
+        try {
+            // Simulate user authentication (replace with actual authentication logic)
+            boolean isAuthenticated = authenticate(username, password);
+
+            if (!isAuthenticated) {
+                throw new AuthenticationException("Authentication failed for user: " + username);
+            }
+
+            // Process user data (replace with actual data processing logic)
+            // ...
+
+        } catch (AuthenticationException e) {
+            // Log the exception, but don't expose sensitive information in the message
+            log("Authentication failed for user: " + username);
+        }
+    }
+
+    // Simulated user authentication (replace with actual authentication logic)
+    private boolean authenticate(String username, String password) {
+        // In a real implementation, check the username and password against a database or authentication service
+        return "correct_username".equals(username) && "correct_password".equals(password);
+    }
+
+    // Log method (replace with your actual logging mechanism)
+    private void log(String message) {
+        System.out.println(message);
+    }
+
+    // Custom exception class for authentication failures
+    private static class AuthenticationException extends RuntimeException {
+        public AuthenticationException(String message) {
+            super(message);
         }
     }
 }
