636814abfcc56b18d8ed38de1a1c0e2b6362ab3f
Balaqiz Orucova <balagiz.orujova1.std@bhos.edu.az>
2023-10-01 17:01:08 +0400

    final

--- src/main/java/org/example/CommandExecutor.java
+++ src/main/java/org/example/CommandExecutor.java
@@ -5,39 +5,23 @@ import java.util.Map;
 
 public class CommandExecutor {
 
-    public void processUserData(String username, String password) {
-        try {
-            // Simulate user authentication (replace with actual authentication logic)
-            boolean isAuthenticated = authenticate(username, password);
+    // Vulnerable method that exposes sensitive data
+    public String retrieveSensitiveData() {
+        String username = "admin";
+        String password = "12345";
 
-            if (!isAuthenticated) {
-                throw new AuthenticationException("Authentication failed for user: " + username);
-            }
-
-            // Process user data (replace with actual data processing logic)
-            // ...
-
-        } catch (AuthenticationException e) {
-            // Log the exception, but don't expose sensitive information in the message
-            log("Authentication failed for user: " + username);
+        // Simulate the exposure of sensitive data in an error message
+        if (!authenticate(username, password)) {
+            throw new RuntimeException("Authentication failed for user: " + username);
         }
+
+        // Return sensitive data (in a real application, this should not happen)
+        return "Welcome, " + username;
     }
 
     // Simulated user authentication (replace with actual authentication logic)
     private boolean authenticate(String username, String password) {
         // In a real implementation, check the username and password against a database or authentication service
         return "correct_username".equals(username) && "correct_password".equals(password);
-    }
-
-    // Log method (replace with your actual logging mechanism)
-    private void log(String message) {
-        System.out.println(message);
-    }
-
-    // Custom exception class for authentication failures
-    private static class AuthenticationException extends RuntimeException {
-        public AuthenticationException(String message) {
-            super(message);
-        }
     }
 }
